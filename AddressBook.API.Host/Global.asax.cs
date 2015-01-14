fb8df66bf646e4168dcfc8b9a4b4c307b0a5f0c1
TemeTDK <temesgenfeseha@gmail.com>
2015-01-14 17:07:12 +0300

    Global Asax tracked

---
+++ AddressBook.API.Host/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using AddressBook.API.DataModel.Types;
+using Funq;
+using ServiceStack.OrmLite;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace AddressBook.API.Host
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
